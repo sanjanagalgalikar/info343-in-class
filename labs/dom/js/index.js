8f0ce6909045ed74a8de01366ef79ec08b88153b
Jessica Basa <jdbasa24@uw.edu>
2017-10-20 13:20:50 -0700

    Added DOM lab solutions

---
+++ labs/dom/js/index.js
@@ -0,0 +1,50 @@
+"use strict";
+
+var boxOne = document.querySelector('#one'),
+	boxTwo = document.querySelector('#two'),
+	boxThree = document.querySelector('#three'),
+	boxFour = document.querySelector('#four'),
+	boxFive = document.querySelector('#five'),
+	boxSix = document.querySelector('#six');
+
+boxOne.addEventListener('click', function (evt) {
+	if (this.classList.contains('spin')) {
+		this.classList.remove('spin');
+	} else {
+		this.classList.add('spin');		
+	}
+});
+
+
+boxTwo.addEventListener('mouseover', function(event) {
+	this.classList.add('fade-to-white');
+	this.classList.add('white');
+});
+
+boxTwo.addEventListener('mouseout', function(event) {
+	this.classList.add('fade-to-red');
+	this.classList.remove('white');
+	this.classList.add('red');
+});
+
+boxThree.addEventListener('dblclick', function(event) {
+	this.classList.add('explode');
+});
+
+boxFour.addEventListener('click', function (evt) {
+	if (this.classList.contains('click')) {
+		this.classList.remove('fall');
+		this.classList.add('float');
+		this.classList.remove('click');		
+	} else {
+		this.classList.remove('float');
+		this.classList.add('fall');
+		this.classList.add('click');
+	}	
+});
+
+boxFive.addEventListener('mouseover', function (evt) {
+	this.classList.add('shake');
+	this.classList.remove('red');
+	this.classList.add('black');
+});
